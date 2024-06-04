2d7fef6e50a78cdfdad483e009936a322039582d
Ledeneva <104145064+Ledeneva@users.noreply.github.com>
2024-06-04 12:28:17 +0300

    create desktop version

---
+++ script.js
@@ -0,0 +1,44 @@
+initMap();
+
+const markerElement = document.createElement('div');
+markerElement.className = 'marker-class';
+markerElement.innerText = "I'm marker!";
+
+const marker = new YMapMarker(
+  {
+    source: 'markerSource',
+    coordinates: [37.588144, 55.733842],
+    draggable: true,
+    mapFollowsOnDrag: true
+  },
+  markerElement
+);
+
+map.addChild(marker);
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [37.588144, 55.733842],
+
+                // Уровень масштабирования
+                zoom: 10
+            }
+        }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
